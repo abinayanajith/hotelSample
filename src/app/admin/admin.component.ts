6344dd449d046b208ba99125c2374e1fb7c3ccbc
abin <abinayanbalarajah.16@cse.mrt.ac.lk>
2021-04-19 01:26:50 +0530

    admin component implemented

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators,FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+
+  hotelForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.hotelForm = this.fb.group({
+      hotelName: [,Validators.required],
+      city:[,Validators.required],
+      country:[,Validators.required],
+      contractStart:[,Validators.required],
+      contractEnd:[,Validators.required],
+      roomDetails: this.fb.array([this.fb.group({roomType:[,Validators.required],price:[,Validators.required],numbers:[,Validators.required],maxAdults:[,Validators.required]})])
+    })
+  }
+
+  callingFunction() {
+    if (this.hotelForm.valid) {
+      console.log("Form Submitted!");
+      this.hotelForm.reset();
+     // this.isSubmitted = true;
+     // this.cd.detectChanges();
+    }
+    //this.isSubmitted = true;
+      //this.cd.detectChanges();
+
+  }
+
+  get searchFormControl() {
+    return this.hotelForm.controls;
+  }
+
+  get roomdetails() {
+    return this.hotelForm.get('roomDetails') as FormArray;
+  }
+
+  deleteSellingPoint(index){
+    
+    this.roomdetails.removeAt(index);
+  }
+
+  addSellingPoint() {
+    this.roomdetails.push(this.fb.group({roomType:[,Validators.required],price:[,Validators.required],numbers:[,Validators.required],maxAdults:[,Validators.required]}));
+   
   }
 
 }
